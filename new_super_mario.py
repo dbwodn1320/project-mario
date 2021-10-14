f5085319dc4805bb604c46f4f0d248f740fb3c57
dbwodn1320 <yjw1320@gmail.com>
2021-10-15 00:02:53 +0900

    마리오 객체 생성
    마리오 스프라이트 작업 및 스프라이트 작동 확인

--- new_super_mario.py
+++ new_super_mario.py
@@ -3,19 +3,19 @@ from pico2d import *
 canvas_width = 1200
 canvas_height = 900
 
-# class Mario:
-#     def __init__(self):
-#         self.x, self.y = 30, 90
-#         self.frameX = 0
-#         self.frameY = 0
-#         self.image = load_image('mario.png')
-#
-#     def update(self):
-#         self.frameX = (self.frameX + 1) % 63
-#         self.x = 0
-#
-#     def draw(self):
-#         self.image.clip_draw(self.frameX*26, 360,26,44,self.x,self.y)
+class Mario:
+    def __init__(self):
+        self.x, self.y = 30, 90
+        self.frameX = 0
+        self.frameY = 0
+        self.image = load_image('mario.png')
+
+    def update(self):
+        self.frameX = (self.frameX + 1) % 63
+        self.x = 0
+
+    def draw(self):
+        self.image.clip_draw(self.frameX*26, 360,26,44,self.x,self.y)
 
 def handle_events():
     global running
@@ -24,13 +24,13 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # elif event.type == SDL_MOUSEMOTION:
-        #     x, y = event.x, canvas_height - 1 - event.y
+         elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, canvas_height - 1 - event.y
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
 
-#mario = Mario()
+mario = Mario()
 open_canvas(canvas_width,canvas_height)
 mario1 = load_image('mario.png')
 x = 30
